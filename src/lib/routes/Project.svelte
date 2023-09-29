c0f86c37b0f74cf5d467e47f388cf311c565cf8a
Antonio Leitao <aml.leitao@campus.fct.unl.pt>
2023-09-29 16:39:01 +0200

    Added splash screen to project page

--- src/lib/routes/Project.svelte
+++ src/lib/routes/Project.svelte
@@ -5,16 +5,16 @@
     import {projects} from "../scripts/store.js";
   document.body.scrollIntoView();
 export let params = {}
-function getTitleByTag(tag) {
+function getProjectByTag(tag) {
     const project = $projects.find(project => project.tag === tag);
-    return project ? project.title : null;
+    return project;
 }
 // Usage
-const project_title = getTitleByTag(params.tag);
+const project = getProjectByTag(params.tag);
 </script>
 
 <div class="top-row">
-  <div class="page-title">{project_title}</div>
+  <div class="page-title">{project.title}</div>
   <a href={"/projects"} use:link>
     <div class="button">
       <!-- <Button icon="arrow left" icon_place="left" text="Home" /> -->
@@ -22,7 +22,11 @@ const project_title = getTitleByTag(params.tag);
     </div>
   </a>
 </div>
-<Papers filter={false} selected_project={params.tag}/>
+<div class="splash">
+    <img src="static/publications/placeholder.jpeg" alt="project figure">
+    <div class="description">{project.description}</div>
+</div>
+<Papers filter={false} selected_project={project.tag}/>
 
 <style>
   .page-title {
@@ -68,5 +72,29 @@ const project_title = getTitleByTag(params.tag);
     font: inherit;
     text-decoration: inherit;
   }
+  .splash{
+      display:flex;
+      flex-direction:column;
+      align-items: center;
+      justify-content: center;
+      width:800px;
+  }
+  .description{
+      font-size:1rem;
+      margin-bottom:1rem;
+  }
 
+  img{
+      width:100%;
+  border: 1px solid var(--clr-lowlight1);
+  border-radius: 0.375rem;
+  padding: 0.375rem;
+  background-color: #FFFFFF;
+  }
+
+@media screen and (max-width: 1024px){
+    .splash{
+    width:89%;
+    }
+}
 </style>
